8622556cafef904bf830c66787cf96e26b51a215
CHJY <jychang@pku.edu.cn>
2024-03-16 23:57:51 +0900

    Create fix-last-update.sh

---
+++ fix-last-update.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+export TZ='Asia/Tokyo'
+git ls-files -z | while read -d '' path; do
+    if [[ $path == source/_posts/* ]]; then
+        touch -d "$(git log -1 --format="@%ct" "$path")" "$path"
+        printf "$(git log -1 --format="%ct" "$path" | xargs -I{} date -d @{} "+%Y-%m-%d %H:%M:%S")"
+        printf \\t
+        echo "$path"
+    fi
+done
