a95ffbe35c8d5d16038ec3cad62201e70dcbcaa8
CHJY <jychang@pku.edu.cn>
2024-03-17 00:14:36 +0900

    修复bug

--- fix-last-update.sh
+++ fix-last-update.sh
@@ -1,9 +1,2 @@
 export TZ='Asia/Tokyo'
-git ls-files -z | while read -d '' path; do
-    if [[ $path == source/_posts/* ]]; then
-        touch -d "$(git log -1 --format="@%ct" "$path")" "$path"
-        printf "$(git log -1 --format="%ct" "$path" | xargs -I{} date -d @{} "+%Y-%m-%d %H:%M:%S")"
-        printf \\t
-        echo "$path"
-    fi
-done
+find source/_posts -name '*.md' | while read file; do touch -d "$(git log -1 --format="@%ct" "$file")" "$file"; done
